4f1ec7228f09f33b35257db5627cd28558ae9ab0
Vijay Birru <vijaykumarbirru876@gmail.com>
2022-06-28 11:24:51 -0500

    poc

---
+++ ConsoleApp1/AzureAuthProvider.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Graph;
+using System;
+using Azure.Identity;
+using System.Collections.Generic;
+//using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace ConsoleApp1
+{
+    class AzureAuthProvider: IAuthenticationProvider
+    {
+        public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+        {
+            try
+            {
+
+                var tenantId = "6764eba9-7437-462d-85a2-06aa757d64f6";
+                var clientId = "d1a0effd-b6bf-4744-ab03-cbc70cbddf71";
+                var clientSecret = "9j08Q~rhU5tylFVkNGDdtxD.QXDu4aHxY5hPPbbS";
+
+                //var tenantId = "2199bfba-a409-4f13-b0c4-18b45933d88d";
+                //var clientId = "a1ff4abd-76b0-41af-a79b-dfff970b7b15";
+                //var clientSecret = "2199bfba-a409-4f13-b0c4-18b45933d88d";
+
+                // Values from app registration
+
+
+
+
+                string resource = "https://graph.microsoft.com";
+                //reading tenant id from JSON file
+                string authority = "https://login.windows.net/" + tenantId;
+
+                AuthenticationContext auth = new AuthenticationContext(authority, false);
+
+                ClientCredential clientCredential = new ClientCredential(clientId, clientSecret);
+
+                var authResult = auth.AcquireTokenAsync(resource, clientCredential).Result;
+
+                var accessToken = authResult.AccessToken;
+                request.Headers.Add("Authorization", "Bearer " + accessToken);
+                request.Headers.Add("Prefer", "outlook.body-content-type='text'");
+            }
+            catch (Exception ex)
+            {
+                //ToDo : Proper exception handling
+                Console.Write("Exception Message: " + ex.Message);
+                Console.Write("\n\nStack Trace: " + ex.StackTrace);
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,52 +10,98 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var tenantId = "6764eba9-7437-462d-85a2-06aa757d64f6";
 
-            // Values from app registration
-            var clientId = "d1a0effd-b6bf-4744-ab03-cbc70cbddf71";
-            var clientSecret = "9j08Q~rhU5tylFVkNGDdtxD.QXDu4aHxY5hPPbbS";
 
-            // using Azure.Identity;
-            var options = new TokenCredentialOptions
+			AzureAuthProvider authProvider = new AzureAuthProvider();
+			GraphServiceClient _graphClient = new GraphServiceClient(authProvider);
+			//var message = new Microsoft.Graph.Message
+			//{
+			//	Subject = "Subject",
+			//	Body = new ItemBody
+			//	{
+			//		ContentType = Microsoft.Graph.BodyType.Html,
+			//		Content = "Test"
+			//	},
+			//	ToRecipients = new List<Recipient>()
+			//		{
+			//		new Recipient
+			//		{
+
+			//			EmailAddress = new Microsoft.Graph.EmailAddress
+			//			{
+			//				Address = "MyGraphUser@outlook.com"
+			//			}
+			//		}
+			//		},
+			//	//HasAttachments = true,
+			//	//Attachments = attachments
+			//};
+
+			var inboxMessages = await _graphClient.Users["Paywise@indot.in.gov"]
+					.MailFolders["inbox"]
+					.Messages
+					.Request()
+					.GetAsync();
+
+			if(inboxMessages.Count > 0)
             {
-                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
-            };
+                foreach (var mailMsg in inboxMessages)
+                {
+					Console.WriteLine("Mail Subject : " + mailMsg.Subject);
+					Console.WriteLine("Mail Body : " + mailMsg.Body.Content);
+				}
+				Console.ReadLine();
+            }
+					//.Wait(8000);
+			//           var tenantId = "6764eba9-7437-462d-85a2-06aa757d64f6";
 
-            // https://docs.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
-            var clientSecretCredential = new ClientSecretCredential(
-                tenantId, clientId, clientSecret, options);
+			//           // Values from app registration
+			//           var clientId = "d1a0effd-b6bf-4744-ab03-cbc70cbddf71";
+			//           var clientSecret = "9j08Q~rhU5tylFVkNGDdtxD.QXDu4aHxY5hPPbbS";
+
+			//           // using Azure.Identity;
+			//           var options = new TokenCredentialOptions
+			//           {
+			//               AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+			//           };
 
 
-			GraphServiceClient graphClient = new GraphServiceClient(clientSecretCredential);
-			
 
-			//await graphClient.Me
-			//	.SendMail(message, null)
-			//	.Request()
-			//	.PostAsync();
-			var message = new Message
-			{
-				Subject = "This is form graphAPI",
-				Body = new ItemBody
-				{
-					ContentType = BodyType.Html,
-					Content = "Hello world!"
-				},
-				ToRecipients = new List<Recipient>()
-	{
-		new Recipient { EmailAddress = new EmailAddress { Address = "AnushaReddy@mycom12.onmicrosoft.com" }}
-	}
-			};
+			//           // https://docs.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+			//           var clientSecretCredential = new ClientSecretCredential(
+			//               tenantId, clientId, clientSecret, options);
 
-			// Send mail as the given user. 
-			graphClient
-				.Users["MyGraphUser@outlook.com"]
-				.SendMail(message, true)
-				.Request()
-				.PostAsync().Wait();
+
+			//		GraphServiceClient graphClient = new GraphServiceClient(clientSecretCredential);
+
+
+			//		//await graphClient.Me
+			//		//	.SendMail(message, null)
+			//		//	.Request()
+			//		//	.PostAsync();
+			//		var message = new Message
+			//		{
+			//			Subject = "Test1",
+			//			Body = new ItemBody
+			//			{
+			//				ContentType = BodyType.Html,
+			//				Content = "Test2"
+			//			},
+			//			ToRecipients = new List<Recipient>()
+			//{
+			//	new Recipient { EmailAddress = new EmailAddress { Address = "MyGraphUser@outlook.com" }}
+			//}
+			//		};
+
+			//		// Send mail as the given user. 
+			//		graphClient
+			//			.Users["5c1f79a7-ca47-4239-bbb9-d14a16a89b02"]
+			//			.SendMail(message, false)
+			//			.Request()
+			//			.PostAsync().Wait();
 		}
     }
 }
+
